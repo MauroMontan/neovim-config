469fbc7248a91507b5e6299245e4cef524c9d89f
MauroMontan <mauroanastacio@outlook.es>
2024-04-03 21:41:20 -0600

    migrating to mason

--- init.lua
+++ init.lua
@@ -1,76 +1,63 @@
 -- packer
-
 require("packer").startup(function(use)
-	use("wbthomason/packer.nvim") -- Package manager
-	use 'rcarriga/nvim-notify'
-	use('MunifTanjim/prettier.nvim')
-	use "lukas-reineke/lsp-format.nvim"
-	use "jose-elias-alvarez/null-ls.nvim"
-	use 'editorconfig/editorconfig-vim'
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use "rafamadriz/friendly-snippets"
-	use("nvim-lua/plenary.nvim")
-	use { "catppuccin/nvim", as = "catppuccin" }
-	use("nvim-telescope/telescope.nvim")
-	use("kyazdani42/nvim-web-devicons")
-	use { 'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp' }
-	use("windwp/nvim-autopairs")
-	use("nvim-lua/popup.nvim")
-	use({ "williamboman/mason.nvim" })
-	use("williamboman/mason-lspconfig.nvim")
-	use("kyazdani42/nvim-tree.lua")
-	use 'folke/lsp-colors.nvim'
-	use 'nvim-treesitter/nvim-treesitter-textobjects'
-	use({ "lewis6991/gitsigns.nvim" })
-	use { 'akinsho/flutter-tools.nvim' }
-	use("neovim/nvim-lspconfig")
-	use("onsails/lspkind-nvim")
-	use("hrsh7th/nvim-cmp")        -- Autocompletion plugin
-	use("hrsh7th/cmp-nvim-lsp")    -- LSP source for nvim-cmp
-	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
-	use("L3MON4D3/LuaSnip")        -- Snippets plugin
-	use 'nvim-lualine/lualine.nvim'
-	use("folke/trouble.nvim")
+  use "rebelot/kanagawa.nvim"
+  use("wbthomason/packer.nvim") -- Package manager
+  use "lukas-reineke/lsp-format.nvim"
+  use "jose-elias-alvarez/null-ls.nvim"
+  use 'editorconfig/editorconfig-vim'
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use "rafamadriz/friendly-snippets"
+  use("nvim-lua/plenary.nvim")
+  use("nvim-telescope/telescope.nvim")
+  use 'nvim-tree/nvim-web-devicons'
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use("kyazdani42/nvim-web-devicons")
+  use { 'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp' }
+  use("windwp/nvim-autopairs")
+  use("nvim-lua/popup.nvim")
+  use({ "williamboman/mason.nvim" })
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use("williamboman/mason-lspconfig.nvim")
+  use("kyazdani42/nvim-tree.lua")
+  use 'folke/lsp-colors.nvim'
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use({ "lewis6991/gitsigns.nvim" })
+  use { 'akinsho/flutter-tools.nvim' }
+  use("neovim/nvim-lspconfig")
+  use("onsails/lspkind-nvim")
+  use("hrsh7th/nvim-cmp")         -- Autocompletion plugin
+  use("hrsh7th/cmp-nvim-lsp")     -- LSP source for nvim-cmp
+  use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+  use("L3MON4D3/LuaSnip")         -- Snippets plugin
+  use 'nvim-lualine/lualine.nvim'
+  use("folke/trouble.nvim")
 end)
 
-
--- prettier setup
-require("prettier").setup()
 
 
 -- lsp format setup
 require("lsp-format").setup {
-	on_attach = function(client)
-		vim.cmd("autocmd BufWritePre * lua vim.lsp.buf.format()")
-	end,
 
-	file_ignore_patterns = {
-		"css"
-	},
+  on_attach = function(client)
+    vim.cmd("autocmd BufWritePre * lua vim.lsp.buf.format()")
+  end,
+
+  file_ignore_patterns = {
+    "css"
+  },
 }
 
--- null ls format
-local null_ls = require("null-ls")
-
-null_ls.setup({
-	on_attach = require("lsp-format").on_attach,
-	sources = {
-		null_ls.builtins.formatting.prettier,
-	},
-})
-
-
--- notify setup
-vim.notify = require("notify")
 
 -- color scheme
-vim.cmd('syntax on')
+vim.cmd.syntax('on')
 
-vim.cmd.colorscheme "catppuccin-frappe"
+--vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd("colorscheme kanagawa-dragon")
 
 
 -- init auopairs
 require('nvim-autopairs').setup {}
+
 
 -- Gui
 vim.o.termguicolors = true
@@ -91,12 +78,12 @@ vim.o.smartcase = true
 
 -- Lua line
 require('lualine').setup({
-	options = {
-		globalstatus = false,
-		ignore_focus = {
-			"NvimTree"
-		},
-	}
+  options = {
+    globalstatus = false,
+    ignore_focus = {
+      "NvimTree"
+    },
+  }
 })
 
 -- Tab config
@@ -104,7 +91,7 @@ require('lualine').setup({
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 -- Navigation
 
@@ -141,53 +128,53 @@ vim.keymap.set("n", "<leader>sp", ":Telescope live_grep <cr>")
 
 --  Trouble
 local signs = {
-	Error = " ",
-	Warn = " ",
-	Hint = " ",
-	Info = " ",
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " ",
 }
 
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 
 -- Lsp colors
 
 require("lsp-colors").setup({
-	Error = "#db4b4b",
-	Warning = "#ff0000",
-	Information = "#0db9d7",
-	Hint = "#10B981"
+  Error = "#db4b4b",
+  Warning = "#ff0000",
+  Information = "#0db9d7",
+  Hint = "#10B981"
 })
 
 
 -- gitsigns
 require("gitsigns").setup({
-	signs = {
-		add = { text = "" },
-		change = { text = "ﰣ" },
-		delete = { text = "" },
-		topdelete = { text = "" },
-		changedelete = { text = "ﰣ" },
-	},
+
+  signs = {
+    add          = { text = '+' },
+    change       = { text = '|' },
+    delete       = { text = '_' },
+    topdelete    = { text = '#' },
+    changedelete = { text = '~' },
+    untracked    = { text = ' ' },
+  },
+
 })
 
 -- NVIM tree
 require("nvim-tree").setup({
-	view = {
-		side = "right",
-	},
+  view = {
+    side = "right",
+  },
 })
-
-
 
 
 -- Completition
 
 local lspkind = require("lspkind")
-
 
 
 -- luasnip setup
@@ -199,56 +186,56 @@ require("luasnip.loaders.from_vscode").lazy_load()
 local cmp = require 'cmp'
 
 cmp.setup {
-	snippet = {
-		expand = function(args)
-			require "luasnip".lsp_expand(args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-	}),
-	sources = cmp.config.sources({
-		{ name = 'luasnip',    option = { show_autosnippets = true } },
-		{ name = 'nvim_lsp' },
-		{ name = 'cmp_tabnine' },
-	}),
+  snippet = {
+    expand = function(args)
+      require "luasnip".lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  }),
+  sources = cmp.config.sources({
+    { name = 'luasnip',    option = { show_autosnippets = true } },
+    { name = 'nvim_lsp' },
+    { name = 'cmp_tabnine' },
+  }),
 
-	formatting = {
-		format = lspkind.cmp_format({
-			mode = 'symbol hello', -- show only symbol annotations
-			--	maxwidth = 250,     -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-			before = function(entry, vim_item)
-				if entry.source.name == 'nvim_lsp' then
-					vim_item.dup = 0
-				end
-				return vim_item
-			end
-		})
-	}
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol hello', -- show only symbol annotations
+      --	maxwidth = 250,     -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      before = function(entry, vim_item)
+        if entry.source.name == 'nvim_lsp' then
+          vim_item.dup = 0
+        end
+        return vim_item
+      end
+    })
+  }
 
 }
 
 -- LSP config
 
 require("mason").setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
 })
 
 
@@ -264,96 +251,137 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsp = require("lspconfig")
 
 lsp.gopls.setup {
-	capabilities = capabilities
+  capabilities = capabilities
 }
 
-
 -- CSS LSP
+--
 lsp.cssls.setup {
-	on_attach = require("lsp-format").on_attach,
-	capabilities = capabilities,
-	single_file_support = true,
-	filetypes = {
-		"css"
-	}
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
+  single_file_support = true,
+  filetypes = {
+    "css"
+  }
 }
 
 
 -- TYPESCRIPT LSP
 lsp.tsserver.setup {
 
-	on_attach = function(client)
-		vim.cmd("<cmd>Prettier")
-		require("lsp-format").on_attach(client)
-	end,
-	capabilities = capabilities,
-	cmd = { "typescript-language-server", "--stdio" },
-	single_file_support = true,
-	filetypes = {
-		"javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "json"
-	}
+  on_attach = function(client)
+    require("lsp-format").on_attach(client)
+  end,
+  capabilities = capabilities,
+  cmd = { "typescript-language-server", "--stdio" },
+  single_file_support = true,
+  filetypes = {
+    "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx",
+    "json"
+  }
 }
 
 
+-- C++
+lsp.clangd.setup {
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities
+}
+
 -- VUE LSP
 lsp.volar.setup {
-	on_attach = require("lsp-format").on_attach,
-	capabilities = capabilities,
-	filetypes = { 'vue' }
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
+  filetypes = { 'vue' }
 
+}
+
+-- go
+
+require 'lspconfig'.gopls.setup {
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
+}
+
+-- python
+lsp.pyright.setup {
+
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
+}
+
+
+-- Astro
+lsp.astro.setup {
+
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
 }
 
 
 -- LUA LSP
 lsp.lua_ls.setup {
 
-	on_attach = require("lsp-format").on_attach,
-	capabilities = capabilities,
-	filetypes = { "lua" },
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
+  filetypes = { "lua" },
+}
+
+
+
+lsp.jdtls.setup {
+
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
+  filetypes = { "java" },
 }
 
 -- RUST LSP
 lsp.rust_analyzer.setup {
-	on_attach = require("lsp-format").on_attach,
-	capabilities = capabilities,
-	filetypes = { "rust" },
-	settings = {
-		['rust-analyzer'] = {
-			diagnostics = {
-				enable = true,
-			}
-		}
-	}
+  on_attach = require("lsp-format").on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+  settings = {
+    ['rust-analyzer'] = {
+      diagnostics = {
+        enable = true,
+      }
+    }
+  }
 }
 
 
 -- Treesitter
 
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all"
-	ensure_installed = { "c", "lua", "rust", "cpp", "swift", "python", "ruby", "dart", "bash", "java" },
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = true,
-	-- Automatically install missing parsers when entering buffer
-	auto_install = false,
-	-- List of parsers to ignore installing (for "all")
-	ignore_install = { "vue" },
-	highlight = {
-		-- `false` will disable the whole extension
-		enable = true,
-		disable = {},
-		additional_vim_regex_highlighting = false,
-	},
+  -- A list of parser names, or "all"
+  ensure_installed = {
+    "c", "lua", "rust", "cpp", "swift", "python", "ruby", "dart", "bash", "java"
+  },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "vue" },
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+    disable = {},
+    additional_vim_regex_highlighting = false,
+  },
 })
 
 -- Telescope settings
 require("telescope").setup({
-	defaults = {
-		show_preview = false,
-		file_ignore_patterns = { "ios/", "build/", "node_modules/", ".git/", ".dart_tool/", ".idea/", "macos/" },
-	},
-	extensions = {
-	},
+  defaults = {
+    show_preview = false,
+    file_ignore_patterns = {
+      "ios/", "build/", "node_modules/", ".git/", ".dart_tool/", ".idea/", "macos/","**.class"
+    },
+  },
+  extensions = {
+  },
 })
 
 -- Telescope extensions
